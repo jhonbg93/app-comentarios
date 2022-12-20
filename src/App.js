cab38e91eeef843ae6f81b1419b5b094e4292380
jhonbg93 <jhon.betancourth03@gmail.com>
2022-12-20 16:43:43 -0500

    realizando componente 2:15:54

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio />
+      </div>
+
     </div>
   );
 }
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,8 +1,19 @@
-import { render } from "@testing-library/react";
-import React from "react";
 
-function Testimonio(){
-    render(
-        
+import React from "react";
+import '../estilos/estilos.css'
+
+function Testimonio() {
+    return(
+       <div className="contenedor-testimonio">
+           <img className="imagen-testimonio" src={require("../imagenes/img-shaw.png")} alt="imagen-testimonio" title="imagen-testimonio"/>
+           <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">Shawn Wang en Singapur</p>
+                <p className="cargo-testimonio">Ingeniero de Software en Amazon</p>
+                <p className="texto-testimonio">"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."</p> 
+           </div>
+       </div>
+
     );
-}
+};
+
+export default Testimonio;
